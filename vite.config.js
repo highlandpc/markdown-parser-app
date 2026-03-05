6fbb4f481e78c16c2aa7c0c424d8f9698f917526
Chirag Khachane <chiragkhachane.ck71@gmail.com>
2026-03-05 15:03:38 -0600

    chore: remove stale html2pdf.js optimizeDeps from vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/markdown-parser-app/",
-  optimizeDeps: {
-    include: ["html2pdf.js"],
-  },
 });
